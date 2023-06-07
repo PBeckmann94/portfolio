1623b45e7d01d21c38d69bc25dc7e4ed29fcbbea
Patrick <thebeckmann@hotmail.com>
2023-06-07 15:11:04 +0200

    Added dummytext (will be replace)

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import '../App.css';
 
 const About = () => {
   return (
-    <div id="section2">
-    <div className="about">
-      <h1>About</h1>
-      {/* Other content */}
-    </div>
+    <div className="container">
+      <div className="card2">
+        <h2>Experience</h2>
+        <p>I have 4 years of experience in web development.</p>
+      </div>
+      <div className="card2">
+        <h2>Skills</h2>
+        <p>I am proficient in HTML, CSS, JavaScript, Ruby on Rails and React.</p>
+      </div>
+      <div className="card2">
+        <h2>Projects</h2>
+        <p>I have worked on various projects including e-commerce websites and portfolio sites.</p>
+      </div>
     </div>
   );
 };
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -2,9 +2,27 @@ import React from 'react';
 
 const Projects = () => {
   return (
-    <div className="projects">
-      <h1>Projects</h1>
-      {/* Other content */}
+    <div className="container">
+      <div className="card2">
+        <h2>Styr</h2>
+        <p>During my internship at Rubynor, I had the privilege to work on an exciting and innovative project called Styr. Styr is a cutting-edge application designed to transform the way remote workers manage and track drawings and documents.
+
+With the increasing trend of remote work, traditional methods of managing physical drawings and documents have become cumbersome and inefficient. Styr aims to solve this problem by providing a centralized platform where remote workers can easily access, collaborate on, and track their important project files.
+
+The core functionality of Styr revolves around its intuitive and user-friendly interface, allowing users to upload, organize, and categorize their drawings and documents effortlessly. Whether it's architectural blueprints, engineering schematics, or legal contracts, Styr ensures that remote workers have all the necessary information at their fingertips.</p>
+      </div>
+      <div className="card2">
+        <h2>Portfolio</h2>
+        <p>Within this portfolio, you will find a showcase of my various projects, each demonstrating my proficiency in front-end development, JavaScript, and React. From interactive web applications to responsive websites, each project represents a unique challenge I have embraced and conquered.
+
+One of the key highlights of my portfolio is the seamless user experience it offers. Through the power of React's component-based architecture, I have crafted intuitive and engaging interfaces that captivate users and provide an enjoyable browsing experience. Whether you're accessing my portfolio from a desktop, tablet, or mobile device, rest assured that it will adapt flawlessly to different screen sizes.</p>
+      </div>
+      <div className="card2">
+        <h2>Messaging App</h2>
+        <p>With React Messenger, communication becomes effortless. Utilizing the power of React's component-based architecture, I have crafted an intuitive and user-friendly interface that allows you to send and receive messages with just a few clicks.
+
+Powered by Firebase's real-time database and authentication features, React Messenger ensures instant message delivery and secure user authentication. Say goodbye to delayed messages and hello to real-time conversations that keep you connected at all times.</p>
+      </div>
     </div>
   );
 };
